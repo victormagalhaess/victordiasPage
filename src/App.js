051dc4ee5c7735a95521d413f55fb4cf7ff219b4
Victor Hugo <victormagalhaes00@outlook.com>
2021-01-05 20:26:27 -0300

    Configuração inicial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Main-Container">
+          <h1 className="fontTest">Victor Hugo Faria Dias Magalhaes</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
