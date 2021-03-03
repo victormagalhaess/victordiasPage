04afdabb60be912b313a6d80e4cd936e4716c4f2
Victor Hugo <victormagalhaes00@outlook.com>
2021-03-03 00:57:11 -0300

    corrigindo import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Header from 'shared/Header/'
-import Body from './pages/Home/Components/Body/'
-import Footer from './pages/Home/Components/Footer/'
+import Home from 'Pages/Home'
 import './styles.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Body/>
-      <Footer/>
+      <Home/>
     </div>
   );
 }
