209bee8dcedd385aff62e61503c13b1c7e3b284c
Victor Hugo <victormagalhaes00@outlook.com>
2021-03-08 19:35:04 -0300

    Animação de typing na home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from 'Pages/Home'
 import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+     <Switch>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
