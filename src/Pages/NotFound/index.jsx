83ad2d45ddec8a08af7c2ca57c9e81df04d74d31
Victor Hugo <victormagalhaes00@outlook.com>
2021-06-02 23:03:02 -0300

    Adding 404 page routing

---
+++ src/Pages/NotFound/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from 'shared/Header';
+
+const NotFound = () => {
+  return (
+    <div>
+      <Header />
+      <span>
+        I'm sorry, but the page that you're trying to access can't be found, even by our
+        specialists. While our detective is on the case, try going Home.
+      </span>
+    </div>
+  );
+};
+
+export default NotFound;
