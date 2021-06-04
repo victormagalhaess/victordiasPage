4277d7baae21938ea12522cb45affbd2e91006db
Victor Hugo D. Magalhães <hello@victordias.dev>
2021-06-04 15:03:23 -0300

    [404Page] Adding 404 page and cleaning up

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from 'shared/Header';
+import styles from './styles.module.scss';
 
 const NotFound = () => {
   return (
     <div>
       <Header />
-      <span>
-        I'm sorry, but the page that you're trying to access can't be found, even by our
-        specialists. While our detective is on the case, try going Home.
-      </span>
+      <div className={styles.notFoundContainer}>
+        <h1 className={styles.errorTitle}>404</h1>
+        <h2 className={styles.errorDescription}>Oops! Page not found.</h2>
+        <h3 className={styles.errorText}>The page you're looking for is nowhere to be found</h3>
+        <span className={styles.errorText}>
+          ← <a href="/"> Return to home</a>{' '}
+        </span>
+      </div>
     </div>
   );
 };
